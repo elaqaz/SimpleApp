ea9472ae3646d43f7766d2dc7920114746c3c4ff
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-05-22 12:42:30 +0100

    Add validation and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
+
+
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    user = User.new(name: "Example User", email: "user@example.com")
+    assert user.valid?
+  end
+
+  test "name should be present" do
+    user = User.new(name: "", email: "user@example.com")
+    assert_not user.valid?
+  end
+
+  test "email should be present" do
+    user = User.new(name: "Example User", email: "")
+    assert_not user.valid?
+  end
 end
