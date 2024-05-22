54a690957dccf756b9af38c559549ddacba05252
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-05-22 18:12:26 +0100

    upgrade UI and background

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   get 'about', to: 'pages#about'
-  get 'pages/home'
-  get 'pages/about'
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'contact', to: 'pages#contact'
+  get 'news', to: 'pages#news'
   root 'pages#home'
 end
