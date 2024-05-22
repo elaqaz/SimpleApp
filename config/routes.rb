346282fa30fe597f991d374883ecd7e8bac3be61
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-05-22 14:02:05 +0100

    sticky _footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   get 'about', to: 'pages#about'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'pages/home'
+  get 'pages/about'
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "pages#home"
+  root 'pages#home'
 end
