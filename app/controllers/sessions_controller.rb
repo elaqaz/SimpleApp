2204717afc40c2ac1c72bf8b0b00e544bc00b64f
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-06-17 13:15:07 +0100

    add current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
   def new
   end
@@ -5,23 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in successfully!'
+      reset_session
+      log_in(user)
+      redirect_to user, notice: 'Logged in successfully!'
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      # flash.now[:alert] = 'Invalid email or password'
       render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out successfully!'
-  end
-
-  private
-
-  def session_params
-    params.require(:session).permit(:email, :password)
+    log_out if logged_in?
+    redirect_to root_url, notice: 'Logged out successfully!'
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,20 @@
 Rails.application.routes.draw do
-  # Define the root of the site
-  root 'pages#home'  # Change this to 'home#index' if you have a Home controller
+  root 'static_pages#home' # or your desired root path
   
-  # User signup routes
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  get    'signup', to: 'users#new'
+  post   'signup', to: 'users#create'
   
-  # Session handling routes for login and logout
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  # Resource routes for users and sessions
-  resources :users, only: [:show, :edit, :update, :destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   
-  # Routes for static pages
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  get 'news', to: 'pages#news'
-  
-  # Resource routes for microposts
-  resources :microposts
+  # Optionally, if you need routes for static pages, you can add:
+  get 'about',    to: 'pages#about'
+  get 'contact',  to: 'pages#contact'
+  get 'news',     to: 'pages#news'
 
+  # If you have microposts, you can add:
+  resources :microposts
 end
