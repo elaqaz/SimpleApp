661d84bfedbef7c1e37d888135851838f8a83d93
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-06-18 18:56:45 +0100

    update users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[edit update destroy]
 
+  def index
+    @users = User.all
+  end
+
   # GET /users/new
   def new
     @user = User.new
