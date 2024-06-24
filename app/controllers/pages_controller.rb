bc13db5be7e90829c45b8df7425c931f28b63368
Elzbieta Szymichowska <ela.buu@gmail.com>
2024-06-24 11:48:26 +0100

    feat: Update home view to include micropost form and feed
    - Added _micropost_form.html.erb partial to handle micropost creation form
    - Added _microposts_list.html.erb partial to display a list of microposts
    - Updated home.html.erb to render the micropost form and feed for logged-in users
    - Display a hero section with a welcome message for non-logged-in users
    - Updated PagesController to set necessary instance variables for microposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed.paginate(page: params[:page])
   end
 
   def about
